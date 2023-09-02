98502414fa54c27d6712efddead31cc7359e8d67
anonsas <lukjanov.igr@gmail.com>
2023-09-02 12:51:33 +0300

    refactored code-splitting

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,14 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'layout/Layout/Layout';
 import { Loader } from 'components';
 
-const HomePage = lazy(() => import('../pages/Home/Home.page'));
-const HomePage2 = lazy(() => wait(1000).then(() => import('../pages/Home/Home.page')));
-const RegisterPage = lazy(() => import('../pages/Register/Register.page'));
-const LoginPage = lazy(() => import('../pages/Login/Login.page'));
-const AccountPage = lazy(() => import('../pages/Account/Account.page'));
-const OrderPage = lazy(() => import('../pages/Order/Order.page'));
-const CartPage = lazy(() => import('../pages/Cart/Cart.page'));
-
 const withSuspense = <P extends object>(Component: FC<P>) => {
   return (props: P) => (
     <Suspense fallback={<Loader />}>
@@ -19,30 +11,24 @@ const withSuspense = <P extends object>(Component: FC<P>) => {
   );
 };
 
-const HomePageWithSuspense = withSuspense(HomePage);
-const RegisterPageWithSuspense = withSuspense(RegisterPage);
-const LoginPageWithSuspense = withSuspense(LoginPage);
-const AccountPageWithSuspense = withSuspense(AccountPage);
-const OrderPageWithSuspense = withSuspense(OrderPage);
-const CartPageWithSuspense = withSuspense(CartPage);
+const HomePage = withSuspense(lazy(() => import('../pages/Home/Home.page')));
+const RegisterPage = withSuspense(lazy(() => import('../pages/Register/Register.page')));
+const LoginPage = withSuspense(lazy(() => import('../pages/Login/Login.page')));
+const AccountPage = withSuspense(lazy(() => import('../pages/Account/Account.page')));
+const OrderPage = withSuspense(lazy(() => import('../pages/Order/Order.page')));
+const CartPage = withSuspense(lazy(() => import('../pages/Cart/Cart.page')));
 
 export function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<HomePageWithSuspense />} />
-        <Route path="/register" element={<RegisterPageWithSuspense />} />
-        <Route path="/login" element={<LoginPageWithSuspense />} />
-        <Route path="/account" element={<AccountPageWithSuspense />} />
-        <Route path="/orders" element={<OrderPageWithSuspense />} />
-        <Route path="/cart" element={<CartPageWithSuspense />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/orders" element={<OrderPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Route>
     </Routes>
   );
-}
-
-function wait(seconds: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, seconds);
-  });
 }
